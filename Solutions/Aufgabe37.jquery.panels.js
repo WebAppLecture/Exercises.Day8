751474dfa8e9ead96dc737053ee39a6214cd20a2
Florian Rappl <rappl.florian@gmail.com>
2015-01-29 12:08:59 +0100

    Included solutions

---
+++ Solutions/Aufgabe37.jquery.panels.js
@@ -0,0 +1,46 @@
+﻿;(function($, undefined) {
+	var defaults = {  
+		speed : 200,
+		maxwidth : 360,
+		width : 180,
+		minwidth : 135,
+		maxfont : 150,
+		font : 100,
+		minfont : 50,
+	};
+	
+	$.fn.panels = function(options) {
+		var opt = $.extend(defaults, options);
+		
+		return this.each(function () {
+			var current = $(this);
+			current.children('li:last-child').addClass('last');
+			current.children('li').hover(
+			function() {
+			
+				$(this).stop().animate({
+					width : opt.maxwidth,
+					fontSize : opt.maxfont
+				}, opt.speed)
+				
+				.siblings('li').stop().animate({
+					width : opt.minwidth,
+					fontSize : opt.minfont
+				}, opt.speed);
+			},
+			
+			function() {
+				
+				$(this).stop().animate({
+					width : opt.width,
+					fontSize : opt.font
+				}, opt.speed)
+
+				.siblings('li').stop().animate({
+					width : opt.width,
+					fontSize : opt.font
+				}, opt.speed);
+			});
+		});
+	};
+})(jQuery);
---
+++ Solutions/Aufgabe39.Worker.js
@@ -0,0 +1,38 @@
+﻿var MAX_ITERATION = 768;
+var COLOR = 25;
+
+onmessage = function(event) {
+    var obj = JSON.parse(event.data);
+	var result = [];
+	
+	for(var i = 0, n = obj.width; i < n; i++) {
+		var x = i * (obj.xe - obj.xs) / obj.width + obj.xs;
+		
+		for(var j = 0, m = obj.height; j < m; j++) {
+			var y = j * (obj.ye - obj.ys) / obj.height + obj.ys;
+			var c = MAX_ITERATION - run(x, y) * COLOR;
+            r = Math.max(Math.min(c, 255), 0);
+            g = Math.max(Math.min(c - 255, 255), 0);
+            b = Math.max(Math.min(c - 511, 255), 0);
+            result.push([i, j, r, g, b]);
+		}
+		
+		postMessage(JSON.stringify(result));
+		result = [];
+	}
+};
+
+function run(x0, y0) {
+	var x = 0,
+		y = 0,
+		iteration = 0;
+
+	while (x * x + y * y < 4 && iteration < MAX_ITERATION) {
+		var xt = x * x - y * y + x0;
+		y = 2 * x * y + y0;
+		x = xt;
+		++iteration;
+	}
+	
+	return iteration;
+}
